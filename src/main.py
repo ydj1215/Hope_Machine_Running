72866123938e541c55ef31c7b7f5fe5ee9088837
ydj1215 <ydjdj2@naver.com>
2024-01-25 17:15:41 +0900

    240125 1715 ydj

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
 from flask import Flask
-from flask_apscheduler import APScheduler
 from flask_cors import CORS
+from 기대_수명_예측 import life_expectancy
 
+app = Flask(__name__)
+CORS(app, origins=['http://localhost:3000'])
+
+app.add_url_rule('/python/life_expectancy', 'life_expectancy', life_expectancy, methods=['POST'])
+
+if __name__ == '__main__':
+	app.run(debug=True)
